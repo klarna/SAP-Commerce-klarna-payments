6bfcc5f214bc0ee7a314346e70e430d66c1e7bcd
anoopktryzens <anoop.k@tryzens.com>
2026-02-24 21:48:23 +0530

    KLARNAPII-2707: Address DTO populator

---
+++ hybris/custom/klarnapayment/src/com/klarna/payment/converter/populator/KlarnaAddressDTOReversePopulator.java
@@ -0,0 +1,41 @@
+package com.klarna.payment.converter.populator;
+
+import de.hybris.platform.commercefacades.user.data.AddressData;
+import de.hybris.platform.commercefacades.user.data.CountryData;
+import de.hybris.platform.commercefacades.user.data.RegionData;
+import de.hybris.platform.converters.Populator;
+import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.klarna.integration.dto.KlarnaAddressDTO;
+
+
+public class KlarnaAddressDTOReversePopulator implements Populator<KlarnaAddressDTO, AddressData>
+{
+
+	@Override
+	public void populate(final KlarnaAddressDTO source, final AddressData target)
+			throws ConversionException
+	{
+		target.setFirstName("Dummy FirstName");
+		target.setLastName("Dummy LastName");
+		target.setLine1((StringUtils.isNotEmpty(source.getStreetAddress())) ? source.getStreetAddress() : "Dummy Line1");
+		target.setLine2((StringUtils.isNotEmpty(source.getStreetAddress2())) ? source.getStreetAddress2() : "Dummy Line2");
+		target.setTown(source.getCity());
+		target.setPostalCode(source.getPostalCode());
+		if (StringUtils.isNotEmpty(source.getCountry()))
+		{
+			final CountryData countryData = new CountryData();
+			countryData.setIsocode(source.getCountry());
+			target.setCountry(countryData);
+		}
+		if (StringUtils.isNotEmpty(source.getRegion()))
+		{
+			final RegionData regionData = new RegionData();
+			regionData.setIsocodeShort(source.getRegion());
+			target.setRegion(regionData);
+		}
+	}
+
+}
