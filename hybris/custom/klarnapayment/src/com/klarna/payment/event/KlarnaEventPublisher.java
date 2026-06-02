2c5459b258a71332189f1c900101d54deef67e0c
anoopktryzens <anoop.k@tryzens.com>
2026-06-02 11:04:32 +0530

    KLARNAPII-2852: Event publisher

---
+++ hybris/custom/klarnapayment/src/com/klarna/payment/event/KlarnaEventPublisher.java
@@ -0,0 +1,30 @@
+package com.klarna.payment.event;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+
+import com.klarna.payment.util.LogHelper;
+
+
+public class KlarnaEventPublisher
+{
+	protected static final Logger LOG = Logger.getLogger(KlarnaEventPublisher.class);
+
+	@Autowired
+	private ApplicationEventPublisher applicationEventPublisher;
+
+	public void publishProperyChangeEvent(final String propertyName, final String oldValue, final String newValue)
+	{
+		LogHelper.debugLog(LOG, "Publishing PropertyChangeEvent for the property :: " + propertyName);
+		try
+		{
+			applicationEventPublisher.publishEvent(new java.beans.PropertyChangeEvent(this, propertyName, oldValue, newValue));
+		}
+		catch (final Exception e)
+		{
+			LOG.error("Proprty Change Event failed due to error :: ", e);
+		}
+	}
+
+}
